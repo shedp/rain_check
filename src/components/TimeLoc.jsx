2615a2f412f1bab028ca9e68082b2d3030a2a5ef
“shedp” <sheenadiep@gmail.com>
2023-10-11 16:43:14 +0100

    add non-functional stome and location component

---
+++ src/components/TimeLoc.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function TimeLoc() {
+    return (
+        <div className="m-4">
+            <div className="flex items-center justify-center my-3">
+                <p className="text-white text-xl font-extralight">
+                    date | time
+                </p>
+            </div>
+            <div className="flex items-center justify-center my-3">
+                <p className="text-white text-xl font-medium">City Name</p>
+            </div>
+        </div>
+    )
+}
+
+export default TimeLoc
