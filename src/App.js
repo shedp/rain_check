88f0fe3cde1ea56ca4e3261a91df146906f6f5d4
“shedp” <sheenadiep@gmail.com>
2023-10-11 15:09:53 +0100

    start react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-2xl font-bold text-red-500">This is my react app</h1>
+      <UilReact size={50} />
+    </div>
+  );
+}
+
+export default App;
