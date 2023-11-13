bd62420a113f6d818fc72626ed877771ac3002ce
“shedp” <sheenadiep@gmail.com>
2023-11-13 23:02:59 +0000

    minor changes to icon sizing

--- src/components/RenderIconOWM.jsx
+++ src/components/RenderIconOWM.jsx
@@ -5,58 +5,58 @@ function RenderIconOWM(props) {
 
     switch (props.icon) {
         case ("01n"):
-            weatherIcon = <UilMoon size={50} className="flex-1" />
+            weatherIcon = <UilMoon size={70} className="flex-1" />
             break;
 
         case ("02d"):
-            weatherIcon = <UilCloudSun size={50} className="flex-1" />
+            weatherIcon = <UilCloudSun size={70} className="flex-1" />
             break;
 
         case ("02n"):
-            weatherIcon = <UilCloudMoon size={50} className="flex-1" />
+            weatherIcon = <UilCloudMoon size={70} className="flex-1" />
             break;
 
         case ("03d"):
         case ("03n"):
-            weatherIcon = <UilCloud size={50} className="flex-1" />
+            weatherIcon = <UilCloud size={70} className="flex-1" />
             break;
 
         case ("04d"):
         case ("04n"):
-            weatherIcon = <UilClouds size={50} className="flex-1" />
+            weatherIcon = <UilClouds size={70} className="flex-1" />
             break;
 
 
         case ("09d"):
-            weatherIcon = <UilCloudSunRain size={50} className="flex-1" />
+            weatherIcon = <UilCloudSunRain size={70} className="flex-1" />
             break;
 
         case ("09n"):
-            weatherIcon = <UilCloudMoonRain size={50} className="flex-1" />
+            weatherIcon = <UilCloudMoonRain size={70} className="flex-1" />
             break;
 
         case ("10d"):
         case ("10n"):
-            weatherIcon = <UilCloudShowersHeavy size={50} className="flex-1" />
+            weatherIcon = <UilCloudShowersHeavy size={70} className="flex-1" />
             break;
 
         case ("11d"):
         case ("11n"):
-            weatherIcon = <UilThunderstorm size={50} className="flex-1" />
+            weatherIcon = <UilThunderstorm size={70} className="flex-1" />
             break;
 
         case ("13d"):
         case ("13n"):
-            weatherIcon = <UilSnowflake size={50} className="flex-1" />
+            weatherIcon = <UilSnowflake size={70} className="flex-1" />
             break;
 
         case ("50d"):
         case ("50n"):
-            weatherIcon = <UilCloudWind size={50} className="flex-1" />
+            weatherIcon = <UilCloudWind size={70} className="flex-1" />
             break;
 
         default:
-            weatherIcon = <UilSun size={50} className="flex-1" />
+            weatherIcon = <UilSun size={70} className="flex-1" />
 
     }
 
