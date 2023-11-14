8e24f033fa5ffeb14f80b661c8d21a6b641343a7
“shedp” <sheenadiep@gmail.com>
2023-11-14 21:02:01 +0000

    responsivness changes

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { UilSearch } from '@iconscout/react-unicons'
+import styles from './Inputs.module.css'
 
 function Inputs({ setCity_Name, unit, setUnit }) {
     const [inputValue, setInputValue] = useState('');
@@ -24,15 +25,16 @@ function Inputs({ setCity_Name, unit, setUnit }) {
 
 
     return (
-        <div className="grid grid-cols-5 justify-center ny-6">
+        <div className="justify-center ny-6" id={styles.inputs}>
             <div className='col-span-1'></div>
             <form onSubmit={handleSubmit} className="flex flex-row justify-center ny-6 col-span-3">
-                <div className="flex flex-row items-center justify-center space-x-4">
+                <div className="flex items-center justify-center space-x-4">
                     <input
                         type="text"
-                        className="text-xl font-light p-2 px-6 w-96 shadow-xl focus:outline-none capitalize placeholder:lowercase rounded-3xl"
+                        className="text-xl font-light p-2 px-6 shadow-xl focus:outline-none capitalize placeholder:lowercase rounded-3xl"
                         placeholder="Search a city..."
                         value={inputValue}
+                        id={styles.inputBar}
                         onChange={handleInputChange}
                     />
                     <button type="submit" className="text-white cursor-pointer transition ease-out hover:scale-125">
@@ -40,7 +42,7 @@ function Inputs({ setCity_Name, unit, setUnit }) {
                     </button>
                 </div>
             </form>
-            <div className="flex flex-row items-center justify-start capitalize col-span-1">
+            <div className="flex flex-row items-center justify-start capitalize col-span-1" id={styles.unitBtn}>
                 <button onClick={handleUnitChange} name="metric" className="text-white text-xl font-light p-2 hover:font-bold">°C</button>
                 <p className="text-white text-xl font-light m-1">|</p>
                 <button onClick={handleUnitChange} name="imperial" className="text-white text-xl font-light p-2 hover:font-bold">°F</button>
